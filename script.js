0fd1cbdc97b50a1c3df69df8682746d08bba1f6c
Nikesh Borkar <99666800+NikeshBorkar@users.noreply.github.com>
2024-07-02 20:26:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let ans="#"
+	if(shortHex[0]=="#"){	
+		for(let i=1;i<shortHex.length;i++){
+			ans=ans+shortHex[i]+shortHex[i]
+		}
+	}else{
+		for(let i=0;i<shortHex.length;i++){
+			ans=ans+shortHex[i]+shortHex[i]
+		}
+	}
+	return ans;   
 };
 
 // Do not change the code below.
